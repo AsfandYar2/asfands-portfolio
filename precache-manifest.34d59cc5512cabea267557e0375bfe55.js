eb9882290bb11a04e376d432da43b77667418925
Asfand Yar <asfi.official@gmail.com>
2019-12-18 14:46:37 +0500

    Updates

---
+++ precache-manifest.34d59cc5512cabea267557e0375bfe55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e8553a0ee1d5ac1721cf0353d52dde",
+    "url": "/asfands-portfolio/index.html"
+  },
+  {
+    "revision": "c4662bbcdfac34445271",
+    "url": "/asfands-portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "31729efa195ce36bc7a6",
+    "url": "/asfands-portfolio/static/css/main.944c21e2.chunk.css"
+  },
+  {
+    "revision": "c4662bbcdfac34445271",
+    "url": "/asfands-portfolio/static/js/2.113a577c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/asfands-portfolio/static/js/2.113a577c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31729efa195ce36bc7a6",
+    "url": "/asfands-portfolio/static/js/main.38f94ae1.chunk.js"
+  },
+  {
+    "revision": "8f7b8ed4a214bd164d9f",
+    "url": "/asfands-portfolio/static/js/runtime-main.2851c4b5.js"
+  }
+]);
